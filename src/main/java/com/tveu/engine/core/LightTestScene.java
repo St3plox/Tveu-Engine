eba1d058ed05806917e19490e5e4b77cc2b528e6
Егор Степаненко <for2accclash@gmail.com>
2024-02-22 14:33:22 +0700

    light in progress

---
+++ src/main/java/com/tveu/engine/core/LightTestScene.java
@@ -0,0 +1,55 @@
+package com.tveu.engine.core;
+
+import com.tveu.engine.rendering.Shader;
+
+public class LightTestScene {
+
+    float[] vertices = {
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+
+            -0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f, -0.5f,
+
+            -0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f,
+    };
+
+    Shader lightCubeShader = new Shader("com/tveu/engine/rendering/shaders/light_cube_vertex.glsl", "com/tveu/engine/rendering/shaders/light_cube_fragment.glsl");
+    Shader lighting = new Shader("com/tveu/engine/rendering/shaders/colors_vertex.glsl", "com/tveu/engine/rendering/shaders/colors_fragment.glsl");
+
+    
+}
