65b7038835263cde5e0656988da940ecb9b8483a
Егор Степаненко <for2accclash@gmail.com>
2024-04-09 15:14:13 +0700

    dt windows fix

--- src/main/java/com/tveu/engine/core/utils/Time.java
+++ src/main/java/com/tveu/engine/core/utils/Time.java
@@ -1,11 +1,11 @@
 package com.tveu.engine.core.utils;
 
+import static org.lwjgl.glfw.GLFW.glfwGetTime;
+
 public class Time {
 
-    public static float timeStarted = System.nanoTime();
-
     public static float getTime() {
-        return (float)((System.nanoTime()  - timeStarted) * 1E-9);
+        return (float) glfwGetTime();
     }
 
 }
