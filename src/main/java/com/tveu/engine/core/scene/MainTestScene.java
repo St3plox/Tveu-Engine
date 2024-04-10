5aec101b87bd1d8241845b0680e4531cf7f2811c
Egor Stepanenko <st3pegor@gmail.com>
2024-04-10 15:50:25 +0700

    added 2 cuboids on MainTestScene

--- src/main/java/com/tveu/engine/core/scene/MainTestScene.java
+++ src/main/java/com/tveu/engine/core/scene/MainTestScene.java
@@ -30,15 +30,21 @@ public class MainTestScene extends SingleLightScene {
         GameObject cube = new GameObject(new Transform(new Vector3f(-0.15f, 1.5f, -1.0f)));
         var cubeComponent = new ReactiveCubeComponent(cube);
         cube.addComponent(cubeComponent);
-        ((Transform)cube.transform).scale(new Vector3f(2.0f));
+        ((Transform)cube.transform).scale(new Vector3f(10.0f, 0.1f, 10.0f));
 
+        // Adding default (orange) cube
+        GameObject cube1 = new GameObject(new Transform(new Vector3f(-0.15f, -1.5f, -1.0f)));
+        var cubeComponent1 = new ReactiveCubeComponent(cube1);
+        cube1.addComponent(cubeComponent1);
+        ((Transform)cube1.transform).scale(new Vector3f(10.0f, 0.1f, 10.0f));
+
+        this.addObj(cube1);
         this.addObj(cube);
 
         //Adding light
-        GameObject light = new GameObject(new Transform(new Vector3f(0.15f, -2.5f, 1.0f)));
+        GameObject light = new GameObject(new Transform(new Vector3f(-0.15f, 0, 1.0f)));
         var lightComponent = new LightCubeComponent(light);
         light.addComponent(lightComponent);
-        lightComponent.setColor(new Vector3f(1.0f, 0.1f, 0.1f));
         ((Transform)light.transform).scale(new Vector3f(0.2f));
 
         this.addObj(light);
