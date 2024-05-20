e3dd65ac336ad5d85aa981ac7dcabf4814afe616
St3plox <st3pegor@gmail.com>
2024-05-20 20:27:00 +0700

    Set uniforms transfered

--- src/main/java/com/tveu/engine/rendering/AbstractShader.java
+++ src/main/java/com/tveu/engine/rendering/AbstractShader.java
@@ -1,28 +1,12 @@
 package com.tveu.engine.rendering;
 
-import org.joml.Matrix4f;
-import org.lwjgl.BufferUtils;
-
-import java.nio.FloatBuffer;
-
-import static org.lwjgl.opengl.GL20.*;
-
 public abstract class AbstractShader {
 
     private int ID;
 
     public abstract void use();
 
-    public void setBool(String name, boolean value) {
-        glUniform1i(glGetUniformLocation(ID, name), Boolean.compare(value, false));
-    }
-
-    public void setInt(String name, int value) {
-        glUniform1i(glGetUniformLocation(ID, name), value);
-    }
-
-    public void setFloat(String name, float value) {
-        glUniform1f(glGetUniformLocation(ID, name), value);
+    public void delete() {
     }
 /*
     public void setVec3(String name, float v0, float v1, float v2) {
